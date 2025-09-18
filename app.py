e37e1468ea483d105ef6880333a1d59d911464c8
cedwinpp <cedwinpp@gmail.com>
2025-09-18 13:28:14 +0200

    Fix: Soluciona problema de sesión expirada y activa campo de texto

--- app.py
+++ app.py
@@ -360,3 +360,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Crea las tablas si no existen
     app.run(debug=True, port=8000)
+
+# Force git to detect changes
